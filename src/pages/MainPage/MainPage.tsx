7194e0fdffce16a7cbda9370abfd7863df7e5476
a.lychits <imfckingslave@gmail.com>
2024-02-17 05:48:28 +0300

    main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,11 @@
 
-//import { Button } from '../../components/UI/Button/Button';
+
 import instagram from '../../assets/images/instagram.svg';
 import logo from '../../assets/images/Logo.svg';
 import teamilkImage from '../../assets/images/teaImageHeader.svg';
-import bubleTeaIcon from '../../assets/images/BubbleTea.svg';
+//import bubleTeaIcon from '../../assets/images/BubbleTea.svg';
 import styles from './MainPage.module.css';
+import { Button } from '../../components/UI/Button/Button';
 
 export const MainPage = () => {
     return (
@@ -16,28 +17,29 @@ export const MainPage = () => {
             </div>
             <div className={styles.title}>
                 <h1>
-                Not Your Average <br />Bubble<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tea <img src={bubleTeaIcon} alt="bubleTeaIcon"/>
+                Not Your Average <br />Bubble<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tea 
                 </h1>
                 <h2>
                 Чайная Tea&Milk преветствует вас у себя в гостях!!! Здесь вы можете ознакомиться с нашим меню и актуальными акциями
                 </h2>
             </div>
         </header>
-        {/* 
+        
         <div className={styles.nav}>
            <Button>меню</Button>
            <Button>акции</Button>
            <Button>помощь в выборе напитка</Button>
            <Button>десерты</Button>
         </div> 
-       */}
-
-        <footer className={styles.footer}>
+   
+        <footer className={styles.footer_container}>
+            <div className={styles.footer}>
            
-            <a href='https://www.instagram.com/tea_milk_minsk/'><img src={instagram} alt="instagram" />
-            <p >@tea_milk_minsk</p></a>
-           
+           <a href='https://www.instagram.com/tea_milk_minsk/'><img src={instagram} alt="instagram" />
+           <p >@tea_milk_minsk</p></a>
+       </div>
         </footer>
+        
         </>
         
     );
