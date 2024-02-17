bdeb1d9324ed7755103720637f2016187db6141a
a.lychits <imfckingslave@gmail.com>
2024-02-17 07:27:22 +0300

    main pages fixes

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,8 @@
-
-
 import instagram from '../../assets/images/instagram.svg';
 import logo from '../../assets/images/Logo.svg';
 import teamilkImage from '../../assets/images/teaImageHeader.svg';
 //import bubleTeaIcon from '../../assets/images/BubbleTea.svg';
 import styles from './MainPage.module.css';
-import { Button } from '../../components/UI/Button/Button';
 
 export const MainPage = () => {
     return (
@@ -17,28 +14,31 @@ export const MainPage = () => {
             </div>
             <div className={styles.title}>
                 <h1>
-                Not Your Average <br />Bubble<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tea 
+                Not Your Average <br />Bubble<br /> Tea 
                 </h1>
                 <h2>
                 Чайная Tea&Milk преветствует вас у себя в гостях!!! Здесь вы можете ознакомиться с нашим меню и актуальными акциями
                 </h2>
             </div>
         </header>
-        
-        <div className={styles.nav}>
-           <Button>меню</Button>
-           <Button>акции</Button>
-           <Button>помощь в выборе напитка</Button>
-           <Button>десерты</Button>
-        </div> 
-   
-        <footer className={styles.footer_container}>
-            <div className={styles.footer}>
+
+        <main className={styles.main}>
+            <nav className={styles.nav}>
+                <ul>
+                    <li >меню</li>
+                    <li>акции</li>
+                    <li className={styles.item}>помощь в выборе <br /> напитка</li>
+                    <li>десерты</li>
+                </ul>
+            </nav>
+        </main>
            
+        <footer className={styles.footer}>
            <a href='https://www.instagram.com/tea_milk_minsk/'><img src={instagram} alt="instagram" />
            <p >@tea_milk_minsk</p></a>
-       </div>
         </footer>
+        
+
         
         </>
         
